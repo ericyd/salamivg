3a85b3c9f908fecd04d4a343185b48a73a953436
Eric Dauenhauer <eric@ericyd.com>
2024-01-09 15:17:11 -0600

    format examples

--- examples/concentric-circles.js
+++ examples/concentric-circles.js
@@ -13,24 +13,40 @@ renderSvg(config, (svg) => {
   svg.fill = null
   svg.stroke = '#000'
 
-  svg.circle(circle({
-    x: center.x,
-    y: center.y,
-    radius: hypot(svg.width, svg.height) * 0.04,
-    'stroke-width': 1
-  }))
+  svg.circle(
+    circle({
+      x: center.x,
+      y: center.y,
+      radius: hypot(svg.width, svg.height) * 0.04,
+      'stroke-width': 1,
+    }),
+  )
 
   const nRings = 14
   for (let i = 1; i <= nRings; i++) {
-    const baseRadius = map(0, Math.log(nRings), hypot(svg.width, svg.height) * 0.09, hypot(svg.width, svg.height) * 0.3, Math.log(i))
-    const sineInfluence = map(0, Math.log(nRings), baseRadius * 0.01, baseRadius * 0.1, Math.log(i))
+    const baseRadius = map(
+      0,
+      Math.log(nRings),
+      hypot(svg.width, svg.height) * 0.09,
+      hypot(svg.width, svg.height) * 0.3,
+      Math.log(i),
+    )
+    const sineInfluence = map(
+      0,
+      Math.log(nRings),
+      baseRadius * 0.01,
+      baseRadius * 0.1,
+      Math.log(i),
+    )
     svg.path((p) => {
       p.strokeWidth = map(1, nRings, 0.3, 0.05, i)
       let radius = baseRadius + Math.sin(0) * baseRadius * 0.1
       p.moveTo(vec2(Math.cos(0) * radius, Math.sin(0) * radius).add(center))
       for (let angle = 0; angle <= Math.PI * 2; angle += 0.05) {
         radius = baseRadius + Math.sin(angle * 6) * sineInfluence //baseRadius * 0.1
-        p.lineTo(vec2(Math.cos(angle) * radius, Math.sin(angle) * radius).add(center))
+        p.lineTo(
+          vec2(Math.cos(angle) * radius, Math.sin(angle) * radius).add(center),
+        )
       }
       p.close()
     })
